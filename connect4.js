63590592a8a3e5bbaf3177275815d72cdd8036e1
conradyak <guy.ben.brown@gmail.com>
2015-10-16 16:12:24 -0400

    one Js code

--- connect4.js
+++ connect4.js
@@ -1,1 +1,9 @@
 console.log("BOOOOOOO!!!!!")
+
+//$('section').bind('click', function() {
+  //  alert($(this).attr('.column'));
+//});
+
+$(".column").click(function(){
+      console.log(event.target.parentNode)
+});
